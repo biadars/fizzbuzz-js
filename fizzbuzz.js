ef3d7cadbeee9479e5cd65cf3d02a624193c9cae
biadars <bianca.darolti@softwire.com>
2021-06-16 18:01:02 +0100

    Added Fizz and Buzz rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,22 @@
 // Here, we create our main function.
 function fizzbuzz() {
     
-    console.log('Welcome to FizzBuzz!');
+    for (let n = 1; n <= 100; n++) {
+        console.log(mapNumber(n));
+    }
+}
 
-    // Put your code here...
-
-    
+function mapNumber(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+        return 'FizzBuzz';
+    }
+    if (n % 3 === 0) {
+        return 'Fizz';
+    }
+    if (n % 5 === 0) {
+        return 'Buzz';
+    }
+    return n;
 }
 
 // Now we run the main function...
